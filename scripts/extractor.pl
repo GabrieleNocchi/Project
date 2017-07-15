f4b4651723688de036795cb090c63859cc3ff018
GabrieleNocchi <g.nocchi@hotmail.com>
2017-07-15 16:17:14 +0100

    Update extractor.pl

--- scripts/extractor.pl
+++ scripts/extractor.pl
@@ -2,6 +2,13 @@ use strict;
 use warnings;
 
 
+# This script filters the outputs of SwiSpot and produces a file
+# with the name of the sequences which passed the filtering.
+# the file is then read by bacfold.pl and bacfold2.pl
+# Note this script is used separately on the output of SwiSpot for the sequences
+# filtered by SD and those filtered by Terminators
+# The filtering changes between the 2 runs, as explained in the next comment
+
 open(INFILE, "temp.txt")
     or die "Can't open file\n";
 
@@ -14,6 +21,8 @@ my $score;
 
 while (my $line = <INFILE>) {
 
+# the code below is to filter by degree of terminator sequestration; to filter by degree of
+# SD sequestration "iT" must be replaced with "iSD";
 
     if ($line =~ /iT: (\d\.\d+)/)  {
        $score = $1;
